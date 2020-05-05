064f245f5a74ec4b00318b265979ef982255a9be
= <=>
2020-05-05 09:13:40 -0700

    Implemented save_frame_as_jpeg in export_handler

--- export_handler.py
+++ export_handler.py
@@ -1,14 +1,26 @@
 import PIL
-import cv2
+from PIL import Image
+from cv2 import cv2
+import ffmpeg
+from io import BytesIO
 
-def return_frame(videopath,frame_number):
-    cap = cv2.VideoCapture(videopath)
-    cap.set(cv2.CV_CAP_PROP_POS_FRAMES, frame_number-1)
-    res, frame = cap.read()
+def save_frame_as_jpeg(in_filename, frame_num,out_filename):
+    out, err = (
+        ffmpeg
+        .input(in_filename)
+        .filter('select', 'gte(n,{})'.format(frame_num))
+        .output('pipe:', vframes=1, format='image2', vcodec='mjpeg')
+        .run(capture_stdout=True)
+    )
+    file_jpgdata = BytesIO(out)
+    frame = Image.open(file_jpgdata)
+    frame.save(out_filename)
 
 
 def main(asset_list):
-    return_frame(asset_list[0].source,asset_list[0].timestamp)
+    #save_frame_as_jpeg('/home/jacobhansen/FeedNet V1/train/raw/GH010001.MP4',500,'/home/jacobhansen/export_frames/test.jpg')
+    pass
+
 
 
 
